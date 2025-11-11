1ed9f894f9729bcf65b3e2dd325062f56970cba0
GeunYeong <jgy091411@gmail.com>
2025-11-11 15:39:41 +0900

    :recycle: chore : RsData -> ResponseEntity 수정

--- src/main/java/com/back/domain/report/report/controller/ReportController.java
+++ src/main/java/com/back/domain/report/report/controller/ReportController.java
@@ -3,10 +3,10 @@ package com.back.domain.report.report.controller;
 import com.back.domain.report.report.dto.ReportReqBody;
 import com.back.domain.report.report.dto.ReportResBody;
 import com.back.domain.report.report.service.ReportService;
-import com.back.global.rsData.RsData;
 import com.back.global.security.SecurityUser;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,9 +21,9 @@ public class ReportController {
     private final ReportService reportService;
 
     @PostMapping
-    public RsData<ReportResBody> postReport(@Valid @RequestBody ReportReqBody reportReqBody,
-                                            @AuthenticationPrincipal SecurityUser securityUser) {
-        ReportResBody reportRes = reportService.postReport(reportReqBody, securityUser.getId());
-        return RsData.success("신고가 등록되었습니다.", reportRes);
+    public ResponseEntity<ReportResBody> postReport(@Valid @RequestBody ReportReqBody reportReqBody,
+                                                    @AuthenticationPrincipal SecurityUser securityUser) {
+        ReportResBody response = reportService.postReport(reportReqBody, securityUser.getId());
+        return ResponseEntity.ok(response);
     }
 }
